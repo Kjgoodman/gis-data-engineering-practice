59a3706db6a7b1ee5aaaca9eff7ccf7ed0c32d5d
Kyle Goodman <goodman.kyle.j@gmail.com>
2026-05-20 18:44:02 -0700

    Add Oregon Counties query

--- bigquery/practice_queries.sql
+++ bigquery/practice_queries.sql
@@ -7,3 +7,21 @@ FROM
     `bigquery-public-data.geo.us_states`
 
 Where state_name = 'Oregon'
+
+-- Query: Oregon Counties by area
+-- Source: bigquery-public-data.geo_us_boundaries.counties
+-- Purpose: Explore Oregon county boundaries for land ownership analysis
+
+SELECT
+  county_name,
+  county_fips_code,
+  ST_AREA(county_geom) / 1000000 AS area_sq_km
+
+FROM
+  `bigquery-public-data.geo_us_boundaries.counties`
+
+WHERE
+  state_fips_code = '41'
+
+ORDER BY 
+  area_sq_km DESC
